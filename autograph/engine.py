198d41fdf5127c911507aa7c078461bacd11eda1
James Roberts <JRoberts86@gmail.com>
2022-08-22 01:04:55 +0100

    clean up

--- autograph/engine.py
+++ autograph/engine.py
@@ -3,13 +3,13 @@
 import math
 
 class Value:
+    """ Stores a single scalar value and its gradient """
     def __init__(self, data, _children=(), _op='', label=''):
         self.data = data
         self.grad = 0.0
         self._backward = lambda: None
         self._prev = set(_children)
         self._op = _op
-        self.label = label
 
     def __add__(self, other):
         other = other if isinstance(other, Value) else Value(other)
@@ -34,7 +34,7 @@ class Value:
         return out
     
     def __pow__(self, other):
-        assert isinstance(other, (int, float)), "only supporting int/float powers for now"
+        assert isinstance(other, (int, float)), "Only supporting int/float powers for now."
         out = Value(self.data**other, (self,), f'**{other}')
         
         def _backward():
--- autograph/nn.py
+++ autograph/nn.py
@@ -3,13 +3,20 @@
 import random
 from autograph.engine import Value
 
-class Neuron:
+class Module:
+    def zero_grad(self):
+        for p in self.parameters():
+            p.grad = 0.0
+
+    def parameters(self):
+        return []
+
+class Neuron(Module):
     def __init__(self, nin):
         self.w = [Value(random.uniform(-1,1)) for _ in range(nin)]
         self.b = Value(random.uniform(-1,1))
 
     def __call__(self, x):
-        #w * x + b
         act = sum((wi*xi for wi,xi in zip(self.w, x)), self.b)
         out = act.tanh()
         return out
@@ -17,9 +24,9 @@ class Neuron:
     def parameters(self):
         return self.w + [self.b]
 
-class Layer:
-    def __init__(self, nin, nout):
-        self.neurons = [Neuron(nin) for _ in range(nout)]
+class Layer(Module):
+    def __init__(self, nin, nout, **kwargs):
+        self.neurons = [Neuron(nin, **kwargs) for _ in range(nout)]
 
     def __call__(self, x):
         outs = [n(x) for n in self.neurons]
@@ -28,7 +35,7 @@ class Layer:
     def parameters(self):
         return [p for neuron in self.neurons for p in neuron.parameters()]
 
-class MLP:
+class MLP(Module):
     def __init__(self, nin, nouts):
         sz = [nin] + nouts
         self.layers = [Layer(sz[i], sz[i+1]) for i in range(len(nouts))]
