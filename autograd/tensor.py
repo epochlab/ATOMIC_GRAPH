9f5f5852a3303457783d89ab72f7aae85dae5ca5
James Roberts <JRoberts86@gmail.com>
2022-09-17 01:21:14 +0100

    Update tensor build

--- autograd/tensor.py
+++ autograd/tensor.py
@@ -4,29 +4,21 @@ from re import S
 import numpy as np
 
 class Tensor:
-    """ N-dimensional array which stores a scalar, vector
-        or matrix of Tensors. """
+    """ N-dimensional array which stores a scalar [n0], vector [n1]
+        or matrix [n2] of Tensors. """
 
     def __init__(self, data):
         self.data = np.array(data, dtype=np.float32)
         self.grad = None
 
-        self._ctx = None
-
     def __str__(self):
         return "Tensor %r with grad %r" % (self.data, self.grad)
 
-    def __add__(self, other):
-        return Tensor(self.data + other.data)
+    def __add__(self, other): return Tensor(self.data + other.data)
+    def __mul__(self, other): return Tensor(self.data * other.data)
+    def __sub__(self, other): return Tensor(self.data + (-other.data))
 
-    def __mul__(self, other):
-        return Tensor(self.data * other.data)
-
-    def __sub__(self, other):
-        return Tensor(self.data + (-other.data))
-
-    def shape(self):
-        return self.data.shape
+    def shape(self): return self.data.shape
 
     # def backward(self):
     #     print(self.data.shape)
